9ea497fbc33ad8fc8038213b46b69c8ab410d7fa
Wouter Admiraal <wad@wouteradmiraal.com>
2012-07-25 12:46:54 +0200

    Add install file to set module weight. Update block rendering for the tools. Invokes custom hook. Implemented this custom hook.

--- theme/opigno--og-tool.tpl.php
+++ theme/opigno--og-tool.tpl.php
@@ -4,5 +4,5 @@
  */
 ?>
 <div class="og-tool <?php print $og_type; ?>-tool" id="<?php print $machine_name; ?>-tool">
-  <?php print $name; ?>
+  <a href="<?php print $url; ?>"><?php print $name; ?></a>
 </div>
