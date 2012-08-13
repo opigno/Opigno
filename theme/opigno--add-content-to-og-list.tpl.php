32dfb37e44b2a1ec48e9eba612932a906379390f
Wouter Admiraal <wad@wouteradmiraal.com>
2012-08-13 15:30:52 +0200

    Correct Notice in Opigno Assessment. If no quiz available, disable quiz selection. Add user access verification in OG tools. Remove audience selection in OG content nodes.

--- theme/opigno--add-content-to-og-list.tpl.php
+++ theme/opigno--add-content-to-og-list.tpl.php
@@ -6,7 +6,9 @@
 <ul class="opigno-add-content optigno-add-content-to-<?php print $og_type; ?>">
   <?php foreach ($tools as $tool): ?>
     <?php if (isset($tool['create_label']) && isset($tool['create_url'])): ?>
-      <li><a href="<?php print $tool['create_url']; ?>"><?php print $tool['create_label']; ?></a></li>
+      <?php if ($tool['create_url']): ?>
+        <li><a href="<?php print $tool['create_url']; ?>"><?php print $tool['create_label']; ?></a></li>
+      <?php endif; ?>
     <?php endif; ?>
   <?php endforeach; ?>
 </ul>
