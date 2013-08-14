69ac7f8fb852c88ab3964c8d878451939caaf4e3
wadmiraal <wad@wouteradmiraal.com>
2013-08-14 14:03:40 +0200

    Add weight for tools tab. Update default template.

--- templates/opigno--tool.tpl.php
+++ templates/opigno--tool.tpl.php
@@ -10,7 +10,7 @@
  *  - $tool (array containing any information the tool module defines. Unfiltered).
  */
 ?>
-<div class="opigno-tool opigno-<?php print str_replace('_', '-', $machine_name); ?>-tool">
+<div class="opigno-tool opigno-<?php print str_replace('_', '-', $machine_name); ?>-tool opigno-tool-block">
   <div class="opigno-tool-icon">&nbsp;</div>
   <h4 class="opigno-tool-name">
     <?php if (!empty($path)): ?>
