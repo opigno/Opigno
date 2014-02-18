83e608c13cbdbdfd9fcddcb5f0a32f4071e5fd92
wadmiraal <wad@wouteradmiraal.com>
2014-02-18 14:23:23 +0100

    Update navigation styles.

--- modules/scorm/ui/theme/opigno-scorm-ui--player-tree-item.tpl.php
+++ modules/scorm/ui/theme/opigno-scorm-ui--player-tree-item.tpl.php
@@ -1,5 +1,5 @@
-<li class="scorm-ui-sco scorm-ui-player-tree-item" data-sco-id="<?php print $sco->id; ?>" data-sco-can-launch="<?php print (int) !empty($sco->launch); ?>">
-  <?php print check_plain($sco->title); ?>
+<li class="scorm-ui-sco scorm-ui-player-tree-item<?php if (!empty($sco->children)) print ' scorm-ui-sco-aggregation scorm-ui-player-tree-item-has-children'; ?>" data-sco-id="<?php print $sco->id; ?>" data-sco-can-launch="<?php print (int) !empty($sco->launch); ?>">
+  <span class="scorm-ui-sco-title scorm-ui-player-tree-item-title"><?php print check_plain($sco->title); ?></span>
   <?php if (!empty($sco->children)): ?>
     <?php print theme('opigno_scorm_ui__player_tree', array('tree' => $sco->children)); ?>
   <?php endif; ?>
