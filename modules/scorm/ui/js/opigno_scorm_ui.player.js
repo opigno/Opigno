e2e0089d66da09b9a1fccc5fdec844b66730345a
wadmiraal <wad@wouteradmiraal.com>
2014-02-18 13:39:31 +0100

    Update player Logic. Add stub API object for SCORM integration.

--- modules/scorm/ui/js/opigno_scorm_ui.player.js
+++ modules/scorm/ui/js/opigno_scorm_ui.player.js
@@ -62,4 +62,70 @@
     this.iframe.src = Drupal.settings.basePath + OpignoScormUIPlayer.PATH_PATTERN.replace('%sco_id', scoID);
   }
 
+  /**
+   * Implementation of the SCORM API.
+   *
+   * @constructor
+   */
+  var OpignoScormUI2004API = function() { };
+
+  /**
+   * Implements Initialize().
+   */
+  OpignoScormUI2004API.prototype.Initialize = function() {
+    console.log('Initialize');
+  }
+
+  /**
+   * Implements Finish().
+   */
+  OpignoScormUI2004API.prototype.Finish = function() {
+    console.log('Finish');
+  }
+
+  /**
+   * Implements GetValue().
+   */
+  OpignoScormUI2004API.prototype.GetValue = function() {
+    console.log('GetValue');
+  }
+
+  /**
+   * Implements SetValue().
+   */
+  OpignoScormUI2004API.prototype.SetValue = function() {
+    console.log('SetValue');
+  }
+
+  /**
+   * Implements Commit().
+   */
+  OpignoScormUI2004API.prototype.Commit = function() {
+    console.log('Commit');
+  }
+
+  /**
+   * Implements GetLastError().
+   */
+  OpignoScormUI2004API.prototype.GetLastError = function() {
+    console.log('GetLastError');
+  }
+
+  /**
+   * Implements GetErrorString().
+   */
+  OpignoScormUI2004API.prototype.GetErrorString = function() {
+    console.log('GetErrorString');
+  }
+
+  /**
+   * Implements GetDiagnostic().
+   */
+  OpignoScormUI2004API.prototype.GetDiagnostic = function() {
+    console.log('GetDiagnostic');
+  }
+
+  // Make it globally available.
+  window.API = window.API_1484_11 = new OpignoScormUI2004API();
+
 })(jQuery, Drupal, window);
