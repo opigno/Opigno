0f35c821465efe68170c5edf0cae59f7ed452d11
wadmiraal <wad@wouteradmiraal.com>
2014-03-06 14:31:31 +0100

    Update data and paths handled.

--- modules/scorm/quiz/js/opigno_scorm_quiz.js
+++ modules/scorm/quiz/js/opigno_scorm_quiz.js
@@ -9,9 +9,16 @@
 
     attach: function(context, settings) {
       if (window.API_1484_11 !== undefined) {
+        try {
+          // Add '_children' properties, as we cannot set them server-side through PHP.
+          window.API_1484_11.data.cmi.objectives._children = 'id,score,success_status,completion_status,progress_measure,description';
+        }
+        catch (e) { }
+
         // Listen on commit event, so we can store our data.
-        window.API_1484_11.bind('commit', function() {
+        window.API_1484_11.bind('commit', function(value, data) {
           // @todo
+          console.log('commit called')
         });
       }
     }
