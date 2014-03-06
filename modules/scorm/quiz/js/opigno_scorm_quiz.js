1a38cb13fb3113363466206e95b59a540c16345a
wadmiraal <wad@wouteradmiraal.com>
2014-03-06 13:35:48 +0100

    Add Quiz integration module.

---
+++ modules/scorm/quiz/js/opigno_scorm_quiz.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * JS Quiz logic for SCORM player.
+ */
+
+;(function($, Drupal, window, undefined) {
+
+  Drupal.behaviors.opignoScormQuiz = {
+
+    attach: function(context, settings) {
+      // Register our own paths.
+      if (window.API_1484_11 !== undefined) {
+        window.API_1484_11.registerCMIPath({
+          
+        });
+      }
+    }
+
+  };
+
+})(jQuery, Drupal, window);
