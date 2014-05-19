f6e4dde41d5f7ec83f863fb61625ad87e5b52142
wadmiraal <wad@wouteradmiraal.com>
2014-05-19 10:14:53 +0200

    Fix for #2270115.

--- modules/scorm/ui/theme/opigno-scorm-ui--player.tpl.php
+++ modules/scorm/ui/theme/opigno-scorm-ui--player.tpl.php
@@ -1,7 +1,9 @@
 <div class="scorm-ui-player" id="scorm-ui-player-scorm-<?php print $scorm_id; ?>" data-scorm-id="<?php print $scorm_id; ?>">
-  <div class="scorm-ui-player-tree-wrapper">
-    <?php print theme('opigno_scorm_ui__player_tree', array('tree' => $tree)); ?>
-  </div>
+  <?php if (!empty($tree)): ?>
+    <div class="scorm-ui-player-tree-wrapper">
+      <?php print theme('opigno_scorm_ui__player_tree', array('tree' => $tree)); ?>
+    </div>
+  <?php endif; ?>
 
   <div class="scorm-ui-player-iframe-wrapper">
     <iframe src="<?php print url("opigno-scorm/ui/player/sco/{$start_sco->id}"); ?>"></iframe>
